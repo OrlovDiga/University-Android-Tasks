188d3c9b9fd28cd04e3ec5e96da2e25d1286c611
OrlovDiga <privaloffdim@yandex.ru>
2020-09-12 11:49:51 +0300

    first commit

---
+++ app/src/main/java/ru/movage/first_task_layout/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.movage.first_task_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lab_1th_task_1th_var_14th);
+    }
+}
